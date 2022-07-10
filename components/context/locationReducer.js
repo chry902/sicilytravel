a71bd23197a5756b36d999dbc4105895bd5e2881
Christopher <tchry902@gmail.com>
2022-07-10 16:09:09 +0200

    debug

--- components/context/locationReducer.js
+++ components/context/locationReducer.js
@@ -4,7 +4,7 @@ import {
   FETCH_ALL_LOCATION_ERROR,
 } from "./constant";
 
-export default (state, action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_ALL_LOCATION_REQUEST:
       return {
